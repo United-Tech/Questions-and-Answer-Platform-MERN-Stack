f479262640484f18f60bb813f0372dc226382784
hannabate <hmengesha2001@gmail.com>
2023-03-03 19:46:50 -0500

    login page setup

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./Login.css"
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="main">
+      <h3>Login to your account</h3>
+      <p>
+        Don't have an account?{" "}
+        <a href="#" className="create-account-link">
+          Create new account
+        </a>
+      </p>
+      <form>
+        <div>
+          <input type="text" placeholder="Your Email" />
+
+          <input type="password" placeholder="Your Password" />
+          <span className="bi-eye"></span>
+        </div>
+
+        <button className="submit">submit</button>
+        <a href="#" className="create-account-link">
+          Create new account
+        </a>
+      </form>
+    </div>
+  );
 }
 
 export default Login
