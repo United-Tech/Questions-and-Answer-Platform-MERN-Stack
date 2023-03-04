7f73e3f844a299c5fc136b755b241f5a05d0f21d
hannabate <hmengesha2001@gmail.com>
2023-03-04 18:45:07 -0500

    login page updated

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,32 +1,55 @@
-import React from 'react'
-import "./Login.css"
-// import "bootstrap-icons/font/bootstrap-icons.css";
+import React, { useState } from "react";
+import "./Login.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  let passwordInputType;
+  let passwordToggleIconClass;
+
+  if (showPassword) {
+    passwordInputType = "text";
+    passwordToggleIconClass = "bi bi-eye password-toggle-icon";
+  } else {
+    passwordInputType = "password";
+    passwordToggleIconClass = "bi bi-eye-slash password-toggle-icon";
+  }
+
   return (
     <div className="main">
       <h3>Login to your account</h3>
       <p>
-        Don't have an account?{" "}
+        Don't have an account?
         <a href="#" className="create-account-link">
           Create new account
         </a>
       </p>
       <form>
-        <div>
+        <div className="password-input-container">
           <input type="text" placeholder="Your Email" />
 
-          <input type="password" placeholder="Your Password" />
-          <span className="bi-eye"></span>
+          <div className="password-input-wrapper">
+            <input type={passwordInputType} placeholder="Your Password" />
+            <span
+              className={passwordToggleIconClass}
+              onClick={togglePasswordVisibility}
+            ></span>
+          </div>
         </div>
 
         <button className="submit">submit</button>
+
         <a href="#" className="create-account-link">
           Create new account
         </a>
       </form>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
