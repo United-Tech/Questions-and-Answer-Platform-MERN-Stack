b7fc020ce5a2f538f0d0418e87389778bb523d0f
YemiHa <yemi.haile07@gmail.com>
2023-03-07 00:37:31 +0300

    Created Header Component

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from './evangadi-logo-home.png';
+import './Header.css';
 
-const Header = () => {
-  return <div>Header</div>;
-};
-
+function Header() {
+    <div>
+<div className="container">
+    <img src={logo}alt=''/>
+    <Link to={"/"}>home</Link>
+    <Link to={"/how-it-works"}>how it works</Link>
+    <button className="btn btn-lg btn-primary my-3" type="submit">
+          SIGN IN
+        </button>
+</div>
+</div>
+}
 export default Header;
+
