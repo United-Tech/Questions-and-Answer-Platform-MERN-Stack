c67a5ac67fdaa5283cf8568859a91358d0bda303
United IT Solution <unitedtech09@gmail.com>
2023-03-07 14:16:34 -0500

    AskQuestion page fix width

--- client/src/Pages/AskQuestion/AskQuestion.js
+++ client/src/Pages/AskQuestion/AskQuestion.js
@@ -4,43 +4,45 @@ import "./AskQuestion.css";
 
 export default function AskQuestion() {
   return (
-    <div className="container my-5">
-      <div className="d-flex flex-column ms-5 mt-5 mb-2">
-        <h1>Steps to write a good question</h1>
-        <ul className="question_steps">
-          <li>Summerize your problem in a one-line title.</li>
-          <li>Describe your problem in more detail.</li>
-          <li>Describe what you tried and what you expected to happen.</li>
-          <li>Review your question and post it to the site.</li>
-        </ul>
-      </div>
-
-      <form className="d-flex flex-column p-5 question_form  justify-content-between">
-        <div className="linkDiv">
-          <h3>Ask a Public Question</h3>
-
-          <Link to="/" className="linkQuestion text-reset">
-            Go to Question Page
-          </Link>
+    <div className="AskQuestion">
+      <div className="container my-5">
+        <div className="d-flex flex-column ms-5 mt-5 mb-2">
+          <h1>Steps to write a good question</h1>
+          <ul className="question_steps">
+            <li>Summerize your problem in a one-line title.</li>
+            <li>Describe your problem in more detail.</li>
+            <li>Describe what you tried and what you expected to happen.</li>
+            <li>Review your question and post it to the site.</li>
+          </ul>
         </div>
 
-        <input
-          className="question-title"
-          type="text"
-          name="question"
-          Placeholder="Title"
-        />
+        <form className="d-flex flex-column p-5 question_form  justify-content-between">
+          <div className="linkDiv">
+            <h3>Ask a Public Question</h3>
 
-        <textarea
-          className="question-input"
-          placeholder="Question Description..."
-          name="questionDescription"
-        ></textarea>
+            <Link to="/" className="linkQuestion text-reset">
+              Go to Question Page
+            </Link>
+          </div>
 
-        <button className="question-btn btn-lg" type="">
-          Post Your Question
-        </button>
-      </form>
+          <input
+            className="question-title"
+            type="text"
+            name="question"
+            Placeholder="Title"
+          />
+
+          <textarea
+            className="question-input"
+            placeholder="Question Description..."
+            name="questionDescription"
+          ></textarea>
+
+          <button className="question-btn btn-lg" type="">
+            Post Your Question
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
